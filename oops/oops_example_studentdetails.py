8c5f9363610255ede78ceae3fbabb74155bf8989
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-17 09:54:56 +0530

    Update oops_example_studentdetails.py

--- oops/oops_example_studentdetails.py
+++ oops/oops_example_studentdetails.py
@@ -1,26 +1,23 @@
 class student:
     def __init__(self,name,rollno,maths_marks,physics_marks,chem_marks):
                 self.name = name
-                self.rollno = rollno
+                self.rollno = rollno                           # intialized the method 
                 self.maths_marks = maths_marks
                 self.physics_marks = physics_marks
                 self.chem_marks = chem_marks
 
-    def printing(self):                 
-        print(self.name,self.rollno,self.maths_marks,self.physics_marks,self.chem_marks)
-
-class calculations:
-    def summation(self,maths_marks,physics_marks,chem_marks):
-        cal = ( maths_marks + physics_marks + chem_marks ) /3
-        return cal
-
-    def decide(self):
-        if (self.maths_marks>75 and self.physics_marks>75):
-            print("eligible")
-        elif (self.physics_marks>75 and self.chem_marks>75):
-            ( "eligible")
-        elif (self.chem_marks>75 and self.maths_marks>75):
-            print("eligible")
+    def calcualtion(self):
+        if(self.maths_marks>=40 and self.physics_marks>=40 and self.chem_marks>=40):
+            total = self.maths_marks + self.physics_marks + self.chem_marks 
+            avg = total/3
+            print(total)
+            print(avg)
+            if (self.maths_marks>75 and self.physics_marks>75) or (self.physics_marks>75 and self.chem_marks>75) or (self.chem_marks>75 and self.maths_marks>75):
+                print("admission is confirmed")
+            else:
+                print("admission is not confirmed")
         else:
-            print("not eligible")
+             print("fail")
 
+s1 = student("farheen",108,97,94,95)
+s1.calcualtion()
