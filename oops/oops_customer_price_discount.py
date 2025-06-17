5a25239aaee1386ff0ab8139a17486f5c61d5d16
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-17 10:46:16 +0530

    Update oops_customer_price_discount.py

--- oops/oops_customer_price_discount.py
+++ oops/oops_customer_price_discount.py
@@ -1,4 +1,8 @@
-#customer and no of items and price of the items and total after take a discount of 20% and print if total is greater that > 200:
+'''customer and no of items and price of the items and total after take a discount of 20% and print if total is greater that > 200:
+Represent the name of the customer and the number of items purchased by the customer in the store using a class 
+As per the no.of itens read the prices and cal the total price'''
+
+If the totalprice is 1200 rupees provide a discount of 20% if not no discount
 class customer:
     def __init__(self,name,no_of_items):
                 self.name = name
