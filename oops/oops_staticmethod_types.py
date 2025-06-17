422df47046b2f9878e29ac636aad298e1b3d27c4
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-17 15:21:09 +0530

    Update and rename oops_staticmethod.py to oops_staticmethod_types.py

--- oops/oops_staticmethod_types.py
+++ oops/oops_staticmethod_types.py
@@ -1,5 +1,15 @@
+# static :
+# using static method
 class mathematics:
     def addnumbers(x,y):
         return x+y
 m = staticmethod(mathematics.addnumbers)     #static method syntax --- staticmethod(class_name.function_name)  result is stored in m
 print(m(4,44))
+
+
+#static method by using decorators
+class mathematics:
+    @staticmethod
+    def addnumbers(x,y):
+        return x+y
+print(mathematics.addnumbers(4,44))
