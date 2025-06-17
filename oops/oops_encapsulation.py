48bd11dcb04ff8f8886afb2c20df018f56636ad5
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-17 14:52:24 +0530

    Create oops_encapsulation.py

---
+++ oops/oops_encapsulation.py
@@ -0,0 +1,11 @@
+class base:
+    def __init__(self):
+        self.__a = 32          #encapsulation both data and methods are combined as a single part is called encapsulation
+        print(self.__a)
+class derived(base):
+    def __init__(self):
+        def __init__(self):
+            base,__init__(self)
+            print(self.__a+2)
+
+b1 = base() 
