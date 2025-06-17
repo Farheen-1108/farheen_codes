ad58478d8d004839cdd9130f642162f5f34c7179
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-17 12:40:54 +0530

    Create oops_mro(methodresolutionorder).py

---
+++ oops/oops_mro(methodresolutionorder).py
@@ -0,0 +1,31 @@
+#MRO (METHOD RESOLUTION ORDER)
+
+class A:
+    def myname(self):                  #self is same for all threee so it is called as hieraracy
+        print("i am class A")
+class B(A):                            # myname is same for all three so it is called as method resolution order
+    def myname(self):
+        print("i am class B")                  
+class C(A):
+    def myname(self):
+        print("i am class C")
+class D(B,C):                          # both B,C is given in calling function so it is multiple
+    pass                               # as we got the both multiple and hieraracy so it is also hybrid inheritance
+d.myname()
+
+
+
+class A: 
+    def myname(self):
+        print("i am class A")
+class B(A):
+    def myname(self):
+        print("i am class B")
+class C(A):
+    def myname(self):
+        print("i am class C")
+class D(B,C):
+    pass
+d = D()
+print(D.__mro__)                     # tuple
+print(D.mro())                       #list
