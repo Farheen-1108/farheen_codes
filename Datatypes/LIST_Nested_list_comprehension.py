877af2676daee49cb2b1dda75a21ba1b3b22fa2c
Farheen-1108 <farheenmohammad2004@gmail.com>
2025-06-28 16:37:13 +0530

    Add files via upload

---
+++ Datatypes/LIST_Nested_list_comprehension.py
@@ -0,0 +1,15 @@
+r,c = map(int,input().split())
+x = [[int(input()) for j in range(c)] for i in range(r)]
+print (x)
+
+# 2 rows and 3 columns 
+'''
+2 3
+2
+5
+7
+1
+6
+7
+[[2, 5, 7], [1, 6, 7]]
+'''
